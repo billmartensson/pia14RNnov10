ab2cc21e9061957f82d6df166d4b1119f27c8fac
Bill Mårtensson <bill@magictechnology.se>
2025-11-10 13:24:34 +0100

    pia

--- app/(tabs)/funscreen.tsx
+++ app/(tabs)/funscreen.tsx
@@ -1,21 +1,42 @@
-import { Text, View } from "react-native";
+import { Image, Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function FunScreen() {
-    return(
-        <View>
+    return (
+        <SafeAreaView
+            style={{ flex: 1, backgroundColor: "red" }}
+        >
+
+            <Image
+                source={require('@/assets/images/react-logo.png')}
+                style={{
+                    position: "absolute",
+                    width: "100%",
+                    height: "100%",
+                    backgroundColor: "blue",
+                    top: 0,
+                    right: 0
+                }}
+            />
+
+            <Image
+                source={require('@/assets/images/react-logo.png')}
+                style={{
+                    position: "absolute",
+                    width: 100,
+                    height: 100,
+                    backgroundColor: "green",
+                    bottom: 50,
+                    right: 20
+                }}
+            />
+
+
+
             <Text>Fun fun fun</Text>
             <Text>mera text</Text>
             <Text>mera text</Text>
-            <Text>mera text</Text>
-            <Text>mera text</Text>
-            <Text>mera text</Text>
-            <Text>mera text</Text>
-            <Text>mera text</Text>
-            <Text>mera text</Text>
-            <Text>mera text</Text>
-            <Text>mera text</Text>
-            <Text>mera text</Text>
 
-        </View>
+        </SafeAreaView>
     )
 }
