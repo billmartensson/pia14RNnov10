099323824b09efefe315773aba50051bb00022ca
Bill Mårtensson <bill@magictechnology.se>
2025-11-10 10:16:14 +0100

    start

---
+++ app/(tabs)/funscreen.tsx
@@ -0,0 +1,21 @@
+import { Text, View } from "react-native";
+
+export default function FunScreen() {
+    return(
+        <View>
+            <Text>Fun fun fun</Text>
+            <Text>mera text</Text>
+            <Text>mera text</Text>
+            <Text>mera text</Text>
+            <Text>mera text</Text>
+            <Text>mera text</Text>
+            <Text>mera text</Text>
+            <Text>mera text</Text>
+            <Text>mera text</Text>
+            <Text>mera text</Text>
+            <Text>mera text</Text>
+            <Text>mera text</Text>
+
+        </View>
+    )
+}
